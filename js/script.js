b12977392a18629ee206b2eaaae2f97eec67c310
francescovalenti <francescovalenti83@outlook.it>
2025-02-26 19:58:32 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// chiediamo all'utente di inserire una parola per vedere se e palindroma
+
+
+
+function isPalindroma(parola) {
+    let parolaInversa = "";
+    for (let i = parola.length - 1; i >= 0; i--) {
+      parolaInversa += parola[i];
+    }
+    return parola === parolaInversa;
+  }
+  
+  const parolaUtente = prompt("Inserisci una parola:");
+  if (isPalindroma(parolaUtente)) {
+    alert("La parola è palindroma.");
+  } else {
+    alert("La parola non è palindroma.");
+  }
