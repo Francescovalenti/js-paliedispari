0b207746041308e3ff2c0ebe313da61e2f92fe53
francescovalenti <francescovalenti83@outlook.it>
2025-02-26 20:19:31 +0100

    feat:2

--- js/script.js
+++ js/script.js
@@ -16,3 +16,29 @@ function isPalindroma(parola) {
   } else {
     alert("La parola non è palindroma.");
   }
+// Funzione per generare un numero casuale da 1 a 6
+  function generaNumeroRandom() {
+    return Math.floor(Math.random() * 6) + 1;
+  }
+  // Funzione per determinare se un numero e pari
+  function isPari(numero) {
+    return numero % 2 === 0;
+  }
+  // esempi di utilizzo
+  const sceltaUtente = prompt("Scegli: pari o dispari?");
+  const numeroUtente = parseInt(prompt("Inserisci un numero da 1 a 6:"));
+  const numeroComputer = generaNumeroRandom();
+  const somma = numeroUtente + numeroComputer;
+  
+  console.log("Numero utente:", numeroUtente);
+  console.log("Numero computer:", numeroComputer);
+  console.log("Somma:", somma);
+  //scelte in base ai numeri scritti in maniera precedente e quello del computer
+  if (
+    (sceltaUtente === "pari" && isPari(somma)) ||
+    (sceltaUtente === "dispari" && !isPari(somma))
+  ) {
+    alert("Hai vinto!");
+  } else {
+    alert("Hai perso!");
+  }
